ca4f7f7172e4315b0bb16e21dd9b95866d3bbf99
Kobe <kobenorcross@gmail.com>
2021-08-10 18:34:30 -0700

    Updated to have a UI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 int main() {
 
+	int userInput = 0;
+	Tree tree = new Tree();
+
+	do {
+		std::cout << "0. Quit program" << std::endl;
+		std::cout << "1. Display Tree" << std::endl;
+		std::cout << "2. Find Object" << std::endl;
+	} while (userInput != 0);
 
 	return 0;
 }
